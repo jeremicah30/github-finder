b0be3b30dbeb85c3cc570a64bb7d7e887cc7f1cc
jeremicah30 <maigabie17@gmail.com>
2022-09-15 16:44:18 +0800

    Not found page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, About, NotFound } from "./pages";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/about" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
